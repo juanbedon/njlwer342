08fad25d1924600c23e135a0c820954c9ac16a63
Juan <juanbedon26@hotmail.com>
2018-06-03 13:20:58 -0500

    Funcionalidad ejercicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { input: '', items: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']};
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.items.map((tarea_por_hacer, index) => <li key={index}>{tarea_por_hacer}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.onSubmit}>
+             <input type="text" id="new-task" value={this.state.input} onChange={this.onChange} placeholder="Ingresa una tarea y oprime Enter"/>
            </form>
         </div>
       </div>
     )
   }
+
+  onChange = (e) => {
+    this.setState({input: e.target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.setState({ input: '', items: [...this.state.items, this.state.input] });
+  }
+
 }
 
 
